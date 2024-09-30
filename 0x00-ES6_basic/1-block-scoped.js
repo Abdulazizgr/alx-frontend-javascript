6f8063e8d30e4000c9e88c0c832e814f1bf353c3
abdulazizgr <abdulazizisa579@gmail.com>
2024-09-30 17:17:56 +0000

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
+
 export default function taskBlock(trueOrFalse) {
-  const task = false;
+  let task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+   
   }
 
   return [task, task2];
